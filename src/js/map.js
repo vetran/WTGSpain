67dff668a623ca2f309544be702d7b39dd67be8d
front <wtg.frontend@gmail.com>
2018-01-29 20:29:08 +0200

    bla bla

--- src/js/map.js
+++ src/js/map.js
@@ -72,147 +72,49 @@ var objects = (function(){
     "use strict";
 
     var doc = document,
-        data = {
-            "stores":[
-                {"id":"0","type":"main","latitude":"50.3994251","longitude":"30.5110194"},
-                {"id":"13","type":"store","latitude":"49.553106","longitude":" 25.570317"},
-                {"id":"12","type":"store","latitude":"49.543134468779385","longitude":" 25.602032435"},
-                {"id":"10","type":"store","latitude":"46.44964484226243","longitude":" 30.6636856030"}
-            ],
-            "cities":[
-                {"id":"3500","latitude":"","longitude":""},{"id":"3495","latitude":"","longitude":""}
-            ]
-        };
+        data = [
+            {
+                lat: '37.829155',
+                lng: '-0.789413',
+                title: 'Бунгало в Сан Педро дель Пинатар Мурсия',
+                image: 'http://wtgspain.com/image/2028878/400x300.jpg'
+            },
+            {
+                lat: '57.829155',
+                lng: '-14.789413',
+                title: 'Сан',
+                image: 'http://wtgspain.com/image/2028878/400x300.jpg'
+            }
+        ];
 
     return {
-        tabsAction: function(){
-            var doc = document, tab = doc.querySelector('.stores__tab'),
-                tabMap = tab.querySelector('.stores__tab-item--map'),
-                tabList = tab.querySelector('.stores__tab-item--list'),
-                contentMap = doc.querySelector('.stores__container'),
-                contentList = doc.querySelector('.stores__list');
+        center: function(){
+            var latitude, longitude;
 
-            tabMap.addEventListener('click', function(){
-                if(!this.classList.contains('active')){
-                    tabMap.classList.toggle('active');
-                    tabList.classList.toggle('active');
-                    contentMap.classList.toggle('active');
-                    contentList.classList.toggle('active');
-                }
-            });
-            tabList.addEventListener('click', function(){
-                if(!this.classList.contains('active')){
-                    tabList.classList.toggle('active');
-                    tabMap.classList.toggle('active');
-                    contentList.classList.toggle('active');
-                    contentMap.classList.toggle('active');
-                }
-            });
-        },
-        moveToStore: function(map){
-            var storeItems = doc.querySelectorAll('.stores__item'),
-                mainStore = doc.querySelector('.stores__main'),
-                _this = this;
+            var lat_max, lat_min, lng_max, lng_min;
 
-            mainStore.addEventListener('click', function(){
-                var id = parseInt(this.dataset.storeId);
 
-                for (var r = 0; r < storeItems.length; r++ ){
-                    if(storeItems[r].classList.contains('active')){
-                        storeItems[r].classList.remove('active');
-                        break
-                    }
-                }
-                mainStore.classList.add('active');
+            for(var i = 0, len = data.length; i < len; i++ ){
 
-                map.setCenter(new google.maps.LatLng(parseFloat(data.stores[0].latitude), parseFloat(data.stores[0].longitude)));
-                map.setZoom(18);
-            });
 
-            for (var s = storeItems.length - 1; s >= 0; s--){
-                storeItems[s].addEventListener('click', function(){
-                    var id = parseInt(this.dataset.storeId);
+            }
+            (lat_max + lat_min) / 2.0
 
-                    for(var d = 0, len = data.stores.length; d < len; d++){
-                        var dataStores = data.stores[d];
-                        if(parseInt(dataStores.id) === id){
-                            mainStore.classList.remove('active');
-                            for (var r = 0; r < storeItems.length; r++ ){
-                                if(storeItems[r].classList.contains('active')){
-                                    storeItems[r].classList.remove('active');
-                                    break;
-                                }
-                            }
-                            this.classList.add('active');
-                            _this.scrollToMap();
-                            map.setCenter(new google.maps.LatLng(parseFloat(dataStores.latitude), parseFloat(dataStores.longitude)));
-                            map.setZoom(18);
-                        }
-                    }
-                });
+            (lng_max + lng_min) / 2.0
+
+            return {
+                lat: -34,
+                lng: 151
             }
         },
-        moveToCity: function(map){
-            var select = doc.querySelector('.stores__select');
-
-            select.addEventListener('change', function(){
-                var storeItems = doc.querySelectorAll('.stores__item'),
-                    mainStore = doc.querySelector('.stores__main'),
-                    id = parseInt(this.value);
-
-                for(var d = 0, len = data.cities.length; d < len; d++) {
-                    var dataCities = data.cities[d];
-
-                    if (parseInt(dataCities.id) === id) {
-                        mainStore.classList.remove('active');
-                        for (var r = 0; r < storeItems.length; r++ ){
-                            if(storeItems[r].classList.contains('active')){
-                                storeItems[r].classList.remove('active');
-                                break
-                            }
-                        }
-                        map.setCenter(new google.maps.LatLng(parseFloat(dataCities.latitude), parseFloat(dataCities.longitude)));
-                        map.setZoom(10);
-                    }
-                }
-            });
-        },
-        showStoreInCity: function(){
-            var stores = doc.querySelectorAll('.stores__item'),
-                select = doc.querySelector('.stores__select'),
-                text = doc.querySelector('.stores__text');
-
-            select.addEventListener('change', function(){
-                if(parseInt(this.value) === 0){
-                    text.classList.remove('hide');
-                }
-                else{
-                    text.classList.add('hide');
-                }
-
-                for (var i = stores.length - 1; i >= 0; i--){
-                    if(parseInt(stores[i].dataset.cityId) == parseInt(this.value)){
-                        stores[i].classList.add('show');
-                    }
-                    else{
-                        stores[i].classList.remove('show');
-                    }
-                }
-            });
-        },
         init: function(){
-            var center, map, icons, markerCluster, mainStore, transitLayer;
+            var center, map, icons, markerCluster, transitLayer, markers = [];
 
-            var path = 'image/map/', markers = [];
-
-            mainStore = data.stores[0];
-
-            center = new google.maps.LatLng(mainStore.latitude, mainStore.longitude);
+            center = new google.maps.LatLng(this.center());
 
             icons = {
-                main: path + 'main.png',
-                store: path + 'store.png',
-                cluster: path + 'pin.png'
+                object: 'src/img/pin.png',
+                cluster: 'src/img/cluster.png'
             };
 
             map = new google.maps.Map(doc.getElementById('stores__map'), {
@@ -227,14 +129,14 @@ var objects = (function(){
             });
 
             for (var i = 0; i < data.stores.length; i++) {
-                var store, marker, coordinates;
+                var obj, marker, coordinates;
 
-                store = data.stores[i];
+                obj = data.stores[i];
 
-                coordinates = new google.maps.LatLng(parseFloat(store.latitude), parseFloat(store.longitude));
+                coordinates = new google.maps.LatLng(parseFloat(obj.latitude), parseFloat(obj.longitude));
                 marker = new google.maps.Marker({
                     animation: google.maps.Animation.DROP,
-                    icon: icons[store.type],
+                    icon: icons.object,
                     position: coordinates
                 });
 
@@ -247,20 +149,15 @@ var objects = (function(){
             markerCluster = new MarkerClusterer(map, markers, {
                 styles: [{
                     url: icons.cluster,
-                    height: 72,
-                    width: 45,
+                    height: 50,
+                    width: 50,
                     anchor: [-25, 0],
-                    textColor: '#ffffff',
+                    textColor: '#618bc5',
                     textSize: 25,
-                    iconAnchor: [22, 72]
+                    iconAnchor: [25, 25]
                 }],
                 gridSize: 80
             });
-
-            this.moveToStore(map);
-            this.moveToCity(map);
-            this.tabsAction();
-            this.showStoreInCity();
         }
     }
 })();
